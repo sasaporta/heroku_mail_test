3f16f0a27e3bb695c387d30a9f0e91f422c81373
Steve Saporta <sasaporta@gmail.com>
2013-01-15 19:09:02 +0000

    do not hard-code username or recipient

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -2,7 +2,7 @@ class MyMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def my_email()
-    mail(to: "saporta98@yahoo.com", subject: "This is a test message")
+    mail(to: ENV['RECIPIENT'], subject: "This is a test message")
   end
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ HerokuTest::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    user_name: "sasaporta@gmail.com",
+    user_name: ENV['SMTP_USER'],
     password: ENV['SMTP_PASSWORD'],
     authentication: "plain",
     enable_starttls_auto: true
